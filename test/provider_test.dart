5acb6a1a51525374f0ab585547b4bd1104f66468
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-18 15:35:28 +0200

    Update provider_test.dart

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -44,3 +44,4 @@ void main() {
           version: 'asv', providerName: 'getbible')!;
       expect(passage.verses!.length, equals(4));
       expect(passage.extra, isNot(null));
+      expect(passage.version, equals('ASV'));
