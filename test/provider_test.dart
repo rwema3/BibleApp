48bcdfe92dea70f5357d90fc6b100b8ae58645a4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-18 15:28:34 +0200

    Update provider_test.dart

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -67,5 +67,4 @@ void main() {
               '[1] In the beginning, God created the heavens and the earth.'));
       expect(passage.version, equals('ESV'));
       passage = await bible.queryPassage(
-        'Genesis 1:1',
     
