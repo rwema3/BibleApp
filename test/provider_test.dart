276a8611abb154413fb3669463fb1305f2ccfba3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-18 15:29:02 +0200

    Update provider_test.dart

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -60,5 +60,4 @@ void main() {
       }
       var passage = await bible.queryPassage('Genesis 1:1',
           providerName: 'esvapi',
-          parameters: {'include-verse-numbers': 'true'})!;
     
