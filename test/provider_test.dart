b5632dad46db678dafceb04a581c5f5f8c2841e9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-18 15:35:44 +0200

    Update provider_test.dart

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -49,3 +49,4 @@ void main() {
       passage = await bible.queryPassage('Genesis 1:1 - 2:3',
           version: 'akjv', providerName: 'getbible')!;
       expect(passage.verses!.length, equals(56));
+      expect(passage.extra, isNot(null));
