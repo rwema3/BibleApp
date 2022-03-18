d853c5f9a113d5e81ffea2a67c9ee18fbbe84d8e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-18 15:28:04 +0200

    Update provider_test.dart

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -73,6 +73,4 @@ void main() {
       expect(passage.passage,
           equals('In the beginning, God created the heavens and the earth.'));
       expect(passage.version, equals('ESV'));
-    });
-
     
